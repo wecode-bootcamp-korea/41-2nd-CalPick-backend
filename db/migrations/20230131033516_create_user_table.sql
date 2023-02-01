0b0e791abb7039383b36c95c5dc908a38a6167ac
Gunho <paulryu1998@gmail.com>
2023-02-01 12:51:53 +0900

    [DB]: 데이터베이스 작업 완료

--- db/migrations/20230131033516_create_user_table.sql
+++ db/migrations/20230131033516_create_user_table.sql
@@ -3,14 +3,13 @@ CREATE TABLE `users` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `email` VARCHAR(50) NOT NULL,
   `password` VARCHAR(200) NULL,
-	`mobile_number` VARCHAR(50) NULL
   `user_name` VARCHAR(50) NULL DEFAULT "USER",
 	`social_pk` VARCHAR(200) UNIQUE NULL,
 	`social_type_id` INT NOT NULL,
 	`mobile_number` VARCHAR(20) NOT NULL,
   `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
   `updated_at` DATETIME ON UPDATE CURRENT_TIMESTAMP,
-	CONSTRAINT users_social_fk FOREIGN KEY users (soical_type_id) REFERENCES social_types (id)
+	CONSTRAINT users_social_fk FOREIGN KEY users (social_type_id) REFERENCES social_types (id)
 	ON DELETE CASCADE
 );
 
