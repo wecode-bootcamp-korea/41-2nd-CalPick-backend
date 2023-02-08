8143a82f543b78562e33becff3c8a991a68e2a60
minkyu_lee <0106995@naver.com>
2023-02-08 15:01:53 +0900

    [41기 이민규] Add: feature/login 소셜로그인 기능구현(google)
    Fix: 코중복 코드 삭제 코드 변수명 변경 및 코드 수정
    
    Fix: http 상태코드 변경

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,8 @@ const router = express.Router();
 
 const userRouter = require("./userRouter");
 const verificationRouter = require("./verificationRouter");
-const testSitesRouter = require("./testsitesRouter");
 
-router.use("/users", userRouter.router);
-router.use("/verification", verificationRouter);
-router.use("/tests", testSitesRouter);
-
+router.use("/user",userRouter);
+router.use("/verification",verificationRouter);
 
 module.exports = router;
