66d5dba7ba2b8fa1220ddd86f3fa18a29e430413
minkyu_lee <0106995@naver.com>
2023-02-08 15:01:53 +0900

    [41기 이민규] Add: feature/login 소셜로그인 기능구현(google)
    Fix: 코중복 코드 삭제 코드 변수명 변경 및 코드 수정
    
    Fix: http 상태코드 변경

--- src/models/userDao.js
+++ src/models/userDao.js
@@ -1,17 +1,38 @@
 const { appDataSource } = require("../databases/database");
 
 const createUser = async(email, password, name) => {
-    return appDataSource.query(
-        `INSERT INTO users(
-            email,
-            password,
-            username
-        )VALUES (?,?,?)
-        `,
-        [email, password, name]
-    );
+	await appDataSource.query(
+		`INSERT INTO users
+			(email, password, username)
+		VALUES 
+			(?,?,?)
+		`,
+		[email, password, name]
+	);
 };
 
+const getUserData = async(email) => {
+	console.log(email)
+	return await appDataSource.query(`
+		SELECT * FROM users
+		WHERE email = ?
+	`, [ email ]
+	);
+}
+
+const createoAuthUser = async (name, email) => {
+	return await appDataSource.query(
+		`INSERT INTO users
+			(email, username)
+		VALUES 
+			(?,?)
+		`,
+		[ email, name ]
+	);
+}
+
 module.exports = {
-    createUser,
+  createUser,
+  getUserData,
+  createoAuthUser,
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,13 +1,22 @@
+const { emailValidationCheck, passwordValidationCheck } = require("../utils/validation-check");
+const { detectError } = require("../utils/detectError");
+
+const path = require("path");
+require("dotenv").config({
+    path: path.resolve(__dirname, "../../.env")
+})
+
+const axios = require("axios");
 const bcrypt = require("bcrypt");
-const response = require("express");
 const jwt = require("jsonwebtoken");
 
 const userDao = require("../models/userDao");
 
-const {
-  emailValidationCheck,
-  passwordValidationCheck,
-} = require("../utils/validation-check");
+const GOOGLE_CLIENT_KEY = process.env.GOOGLE_CLIENT_KEY;
+const GOOGLE_SECRET_KEY = process.env.GOOGLE_SECRET_KEY;
+const REDIRECT_URL = process.env.GOOGLE_REDIRECT_URI;
+const JSON_SECRET_KEY = process.env.JSON_SECRET_KEY;
+
 
 const signUp = async (email, password, name) => {
   await emailValidationCheck(email);
@@ -20,6 +29,56 @@ const signUp = async (email, password, name) => {
   return userDao.createUser(email, hashedPassword, name);
 };
 
+const signIn = async (enteredEmail, enteredPassword) => {
+	const [ user ] = await userDao.getUserData(enteredEmail);
+	if (!user) detectError("NO USER DATA", 404);
+
+	const { id, email, password, name } = user;
+	const passwordsAreEqual = await bcrypt.compare(enteredPassword, password);
+
+	if (!passwordsAreEqual) detectError("PASSWORDS ARE NOT EQUAL", 404);
+	const accessToken = jwt.sign({ "userId": id, "userName": name, "userEmail": email }, JSON_SECRET_KEY);
+
+	return accessToken;
+}
+
+const googleLogin = async (code) => {
+	const res = await axios({
+		url: "https://oauth2.googleapis.com/token",
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		params: {
+			"client_id": GOOGLE_CLIENT_KEY,
+			"client_secret": GOOGLE_SECRET_KEY,
+			"code": code,
+			"grant_type": "authorization_code",
+			"redirect_uri": REDIRECT_URL
+		}
+	}).then((res) => {
+		return res.data;
+	}).catch((err) => {
+		detectError("NOT CONNECTED", 404);
+	})
+
+	if (!res || !res.id_token) detectError("NO AUTH TOKEN", 403);
+	
+	const ID_TOKEN = res.id_token;
+	const { email, email_verified, name} = jwt.decode(ID_TOKEN);
+	
+	if (!email_verified) detectError("NOT VERIFED EMAIL", 401);
+
+	const result = await userDao.createoAuthUser(name, email);
+	const userId = result.insertId;
+
+	const accessToken = jwt.sign({ "userId": userId, "userName": name, "userEmail": email }, JSON_SECRET_KEY);
+	
+	return accessToken;
+}
+
 module.exports = {
-    signUp,
+  signUp,
+	signIn,
+	googleLogin,
 }
